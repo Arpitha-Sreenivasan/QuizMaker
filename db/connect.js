fe4b87e44bfbc402a1ea3d66800bf3d496800723
Vivek Doshi <80559953+vivekdoshi98@users.noreply.github.com>
2022-03-31 12:18:13 +0530

    Other db operations

--- db/connect.js
+++ db/connect.js
@@ -1,53 +1,5 @@
 const {MongoClient} =require('mongodb');
 
-let newQuiz={"id": "ghi789",
-"details": {
-    "title": "Quiz3",
-    "Questions": [
-        {
-            "QueNum": 1,
-            "QuestionText": "What is your name?",
-            "Options": [
-                "option1",
-                "option2",
-                "option3",
-                "option4"
-            ],
-            "Answers": [
-                "option3",
-                "option2"
-            ]
-        },
-        {
-            "QueNum": 2,
-            "QuestionText": "How are you?",
-            "Options": [
-                "option1",
-                "option2",
-                "option3"
-            ],
-            "Answers": [
-                "option2"
-            ]
-        },
-        {
-            "QueNum": 3,
-            "QuestionText": "What is your name?",
-            "Options": [
-                "option1",
-                "option2",
-                "option3",
-                "option4",
-                "option5"
-            ],
-            "Answers": [
-                "option5",
-                "option2"
-            ]
-        }
-    ]
-}}
-
 async function main(){
     const dbpath='mongodb+srv://QuizMaker123:quizpass@quizcluster.fisgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
@@ -55,9 +7,15 @@ async function main(){
     try{
         await client.connect();
         await listDbs(client);
-        //await InsertOneRecord(client,"Users",{email:"ghi@gmail.com",password:"pass1"});
-        //await FindQuizByID(client,"CreateQuiz","Arpitha123");
-        await AddQuizToUser(client,"CreateQuiz",newQuiz,"Arpitha123");
+        await InsertOneRecord(client,"Users",{email:"deleteuser@gmail.com",password:"pass1"});
+        await DeleteRecord(client,{"email":"deleteuser@gmail.com"});
+        
+        await FindQuizByID(client,"CreateQuiz","abc123");
+        await CheckQuizIdExists(client,"abc123");
+        await CheckUserExists(client,"vivek@gmail.com");
+        await GetUserPassword(client,"vivek@gmail.com");
+        await FindAllQuizByUser(client,"vivek@gmail.com","CreateQuiz");
+        await CheckQuizAlreadyAttempted(client,"vivek@gmail.com","ghi789");
     }catch(e){
         console.error(e);
     }
@@ -80,23 +38,66 @@ async function InsertOneRecord(client,table,newRec){
     console.log(res.insertedId);
 }
 
-async function InsertManyRecord(client,table,newRec){
-    const res=await client.db("QuizMaker").collection(table).InsertManyRecord(newRec);
-    console.log(res.insertedId);
-}
-
 async function FindQuizByID(client,table,quizId){
-    const res=await client.db("QuizMaker").collection(table).findOne({ username: quizId});
+    const res=await client.db("QuizMaker").collection(table).findOne({ "id": quizId});
     if(res){
-        console.log("data:"+res.Quizzes[0].id);
+        console.log("data:"+res);
     }
     else{
         console.log("No data found "+res);
     }
 }
 
-//to append quiz to creator can also be used in attempt quiz
-async function AddQuizToUser(client,table,quizObj,name){
-    const res=await client.db("QuizMaker").collection(table).updateOne({username: name},{ $push:{Quizzes: quizObj}});
+async function CheckQuizIdExists(client,quizId){
+    const result=await client.db("QuizMaker").collection("CreateQuiz").findOne({"id":quizId});
+    if(result){
+        console.log("Quiz id exists");
+    }
+    else{
+        console.log("Quiz id does not exists");
+    }
+}
+async function CheckUserExists(client,emailId){
+    const result=await client.db("QuizMaker").collection("Users").findOne({"email":emailId});
+    if(result){
+        console.log("Email id exists");
+    }
+    else{
+        console.log("Email id does not exists");
+    }
+}
 
+async function GetUserPassword(client,emailId){
+    const result=await client.db("QuizMaker").collection("Users").findOne({"email":emailId});
+    if(result){
+        console.log("Password: "+result.password);
+    }
+    else{
+        console.log("Email id does not exists");
+    }
+}
+async function FindAllQuizByUser(client,emailId,table){
+    const result=await client.db("QuizMaker").collection(table).find({"username":emailId});
+    if(result){
+        // result.forEach((r)=>{
+        //     console.log("Quiz: "+r);
+        // });
+        console.log(result);
+    }
+    else{
+        console.log("No quiz found");
+    }
+}
+async function CheckQuizAlreadyAttempted(client,emailId,quizId){
+    const result=await client.db("QuizMaker").collection("AttemptQuiz").findOne({"id":quizId,"username":emailId});
+    if(result){
+        console.log("Quiz already attempted");
+    }
+    else{
+        console.log("Quiz not attempted");
+    }
+}
+async function DeleteRecord(client,query){
+    const result=await client.db("QuizMaker").collection("Users").deleteOne(query);
+    console.log(result);
 }
